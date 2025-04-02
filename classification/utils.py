89f9c662d0637e854b44d0eee1a03c0b1b8a537d
Yegor <yegortr2004@gmail.com>
2025-04-02 23:43:57 +0300

    New experiments + results

--- classification/utils.py
+++ classification/utils.py
@@ -80,6 +80,52 @@ def prepare_wallets_features_data(data, type):
         return res
     raise NotImplementedError
 
+def prepare_wallets_features_data_new(data, type):
+    return data.drop([
+        "userId",
+        "Time step",
+        "addrId",
+    ], axis=1)
+    if type == "raw":
+        return data[COMMON_FEATURES]
+    if type == "with_users":
+        return data[COMMON_FEATURES + USERS_FEATURES]
+    if type == "full":
+        return data[COMMON_FEATURES + USERS_FEATURES + CUSTOM_FEATURES]
+    if type == "full_with_extra_flags":
+        res = data[COMMON_FEATURES + USERS_FEATURES]
+        
+        res["one_ts_active"] = (res["num_timesteps_appeared_in"] == 1).astype(int)
+        res["user_one_ts_active"] = (res["user_active_time_steps_cnt"] == 1).astype(int)
+
+        res["user_one_addr_flg"] = (res["user_addr_cnt"] == 1).astype(int)
+
+        res["no_sending"] = (res["user_outcoming_tx_cnt"] == 0).astype(int)
+        res["one_input_tx"] = (res["num_txs_as_sender"] == 1).astype(int)
+        res["one_output_tx"] = (res["num_txs_as receiver"] == 1).astype(int)
+
+        # res1 = res[res["num_txs_as receiver"] == 0]
+        # res2 = res[res["num_txs_as receiver"] != 0]
+        # res2["input_to_output_tx_ratio"] = (
+        #     res2["num_txs_as_sender"] / res2["num_txs_as receiver"]
+        # ).fillna(0)
+        # res1["input_to_output_tx_ratio"] = 0
+        # res = pd.concat([res1, res2])
+
+        # res["one_receiving"] = (res["user_incoming_tx_cnt"] == 1).astype(int)
+        # res["one_sending"] = (res["user_outcoming_tx_cnt"] == 1).astype(int)
+        # res["one_sending_and_receiving"] = ((res["user_outcoming_tx_cnt"] == 1) & (res["user_incoming_tx_cnt"] == 1)).astype(int)
+        # res["no_receiving"] = (res["num_txs_as receiver"] == 0).astype(int)
+        # res["many_addr_nei"] = (res["transacted_w_address_total"] >= 300)
+        # res["many_input_btc"] = (res["btc_received_total"] >= np.quantile(res["btc_received_total"], 0.95))
+        
+        # res["one_output_and_input_tx"] = ((res["num_txs_as_sender"] == 1) & (res["num_txs_as receiver"] == 1)).astype(int)
+        
+        
+        return res
+    raise NotImplementedError
+
+
 def get_training_data(data_raw, no_unknown=True, binary=True):
     if no_unknown:
         data = data_raw[data_raw["class"] != 3]
@@ -116,7 +162,7 @@ def get_best_threshold(pred_probas, y_true, plot=True, thrs_cnt=20, return_hists
         recall_hist.append(rec)
         f1_hist.append(f1)
         fb_hist.append(fbeta_score(y_true, y_pred, beta=4))
-    best_ind = np.argmax(f1_hist)
+    best_ind = np.argmax(fb_hist)
 
     if plot:
         print(f"""
@@ -171,3 +217,73 @@ def get_addr_position(addrId, wallets_data):
     if test.isin([addrId]).sum():
         return "test", test.index[test == addrId].tolist()[0]
     return None
+
+
+def add_flags(df):
+    df["big_bct_received"] =( df["btc_received_total"] > 10).astype(int)
+    df["big_inp_addr"] =( df["input_address_cnt"] > 20).astype(int)
+
+    return df
+
+def get_prepared_train_test(raw_train, raw_test, type="full"):
+    """
+    type: (full, no_user)
+    """
+    if type == "full":
+        fields_to_exc = [
+            "addrId",
+            "Time step",
+            "first_block_appeared_in",
+            "last_block_appeared_in",
+
+            "incoming_tx_fees_mean",
+
+            "userId"
+        ]
+    elif type == "no_users":
+        users_fields = list(filter( lambda x : "user" in x, list(raw_train.columns)))
+        fields_to_exc = [
+            "addrId",
+            "Time step",
+            "first_block_appeared_in",
+            "last_block_appeared_in",
+            "incoming_tx_fees_mean",
+
+            *users_fields
+
+        ]
+    else:
+        raise Exception("Unknown dataset type")
+
+    raw_train = raw_train.drop(fields_to_exc, axis=1)
+    raw_test = raw_test.drop(fields_to_exc, axis=1)
+    
+    raw_train_prep = add_flags(raw_train)
+    raw_test_prep = add_flags(raw_test)
+    
+    x_train = raw_train_prep.drop("class", axis=1)
+    y_train = (raw_train_prep["class"] == 1).astype(int)
+
+    x_test = raw_test_prep.drop("class", axis=1)
+    y_test = (raw_test_prep["class"] == 1).astype(int)
+
+
+
+    return x_train.reset_index().drop("index", axis=1), \
+        x_test.reset_index().drop("index", axis=1), \
+        y_train.reset_index().drop("index", axis=1)["class"], \
+        y_test.reset_index().drop("index", axis=1)["class"]
+
+
+def get_feature_to_importance(dataset_cols, feature_imps):
+    for ind in np.argsort(feature_imps)[::-1]:
+        print(dataset_cols[ind], feature_imps[ind])
+
+
+def get_best_metric_for_optuna(y_pred, y_true):
+    thrs = np.linspace(0.2, 0.4, 10)
+    metrics_hist = []
+
+    for thr in thrs:
+        metrics_hist.append( fbeta_score(y_true, (y_pred >= thr).astype(int), beta=2) )
+    return max(metrics_hist), thrs[np.array(metrics_hist).argmax()]
